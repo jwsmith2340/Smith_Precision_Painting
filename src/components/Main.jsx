45e1f9ef4012a193fcbf6ae4b9661f6cd715cc72
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2022-06-30 07:56:22 -0400

    Added header components in Main instead of in App.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,20 +1,34 @@
 import { Route, Switch, Redirect } from "react-router-dom";
 import Home from '../pages/Home'
 import About from '../pages/About'
+import Header from '../components/Header'
 
 import '../public/scss/Main.scss'
 
 function Main() {
+
     return (
+
         <Switch>
+
             <Route exact path='/'>
+
+                <Header />
                 <Home />
+
             </Route>
+
             <Route path='/about'>
+
+                <Header />
                 <About />
+
             </Route>
+            
         </Switch>
+
     )
+
 }
 
 export default Main
