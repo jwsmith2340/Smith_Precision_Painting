261b78b930025689015b5b69d2ca4b397a7313ae
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2022-06-30 07:55:37 -0400

    Removed the Header tag from App, moved to Main for better control.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 import Main from './components/Main'
-import Header from './components/Header'
 
 function App() {
   return (
     <>
-      <Header />
+      
       <Main />
     </>
   );
