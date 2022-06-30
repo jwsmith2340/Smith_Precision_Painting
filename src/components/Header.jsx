6f6e8158db86150a345bbbfbef35fffd83e3eb65
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2022-06-30 07:55:06 -0400

    Header divs added to begin populating mobile menu bar with basic scss styling as well.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
 import '../public/scss/Header.scss'
 
+import { FaBars } from "react-icons/fa";
+import { FaPaintRoller } from "react-icons/fa";
+
 function Header() {
-    return <h1>Header</h1>
+    return (
+        <div id="header_box">
+            
+            <div className="mob">
+
+                <div className="mob-menu-btn"><FaBars /></div>
+                <div className="mob-logo"><FaPaintRoller /></div>
+
+            </div>
+
+        </div>
+    )
 }
 
 export default Header
