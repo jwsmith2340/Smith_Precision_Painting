3cdf7422aacca1240a904b8422eb7ec2747ee063
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2022-07-11 16:39:04 -0400

    Insurance jsx and scss files created and set up with small p sections.

---
+++ src/components/Main_Components/Insurance.jsx
@@ -0,0 +1,28 @@
+import '../../public/scss/Insurance.scss'
+
+function Insurance() {
+
+    return (
+
+        <>
+        
+            <div className="insurance_box_mob">
+
+                <h3>Licensed & Insured</h3>
+
+                <p className='insurance_p_statement'>
+
+                    Hiring a painter doesn't have to be a risky endeavor. Smith Precision Painting
+                    is professionally license and insured, so you can be confident that your home is in good hands.
+
+                </p>
+
+            </div>
+
+        </>
+
+    );
+
+}
+
+export default Insurance;
