dcdf7d42243042a7166b293c03cea17ae111dfeb
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2022-07-11 16:41:11 -0400

    Services component text populated, hard coded for now for MVP, will become API call once site is live.

--- src/components/Main_Components/Services.jsx
+++ src/components/Main_Components/Services.jsx
@@ -5,8 +5,17 @@ function Services() {
     return (
 
         <>
-        
-            <h1>Services</h1>
+
+            <div className="services_p_mob">
+
+                <p className='services_p_statement'>Smith Precision Painting offers full service interior house painting. Our professional partnership with 
+                    Benjamin Moore ensures that only the highest quality materials will be used on your project. 
+                    Call us today to schedule a free in house estimate!
+                </p><br></br>
+
+                <p className='services_tel_num'>304-685-5983</p>
+
+            </div>
 
         </>
 
